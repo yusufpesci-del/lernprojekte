1518120b751ca83f6d1675d664393aae8fe43b02
Yusuf Ogul <yusufpesci@gmail.com>
2026-09-18 23:48:49 +0200

    Lerntagebuch 16.09., CS50P Woche 0 begonnen

--- hello.py
+++ hello.py
@@ -4,6 +4,8 @@ name = input("What is your name? ")
 # Remove whitespace from str
 name = name.strip()  
 
+# Capitalize the first letter of the name
+name = name.capitalize()  
 
 # Say hello to the user
 print(f"hello, {name}")
